9a163aab8dede2ce793049a41c1b1e3c738a810b
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-09-18 19:24:25 +0300

    Fix bugs with results. Add our variant input

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,21 +8,38 @@ namespace Lab1
     {
         static void Main()
         {
-            // Input shit to calculate stuff ...
-            var l = new double[] { };
-            var k = new double[] {  };
-            var numberFile = new int[] {  };
+            var k = new double[] { 100, 0, 200, 0, 100, 0, 0, 200, 300, 0};
+            var l = new double[] { 0, 50, 0, 75, 0, 100, 75, 0, 0, 50 };
+            var numberFile = new int[] {1, 0, 2, 0, 0, 0, 0, 2, 3, 0 };
 
             var a = Matrix<double>.Build.DenseOfArray(new double[,]
             {
-                
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, (2 / 7.0) * (1 / 6.0), (2 / 7.0) * (5 / 6.0), 0, (5 / 7.0) * (4 / 5.0), (5 / 7.0) * (1 / 5.0), 0, 0, 0},
+                {0, 0, 0, 0, 0, 4 / 5.0, 1 / 5.0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 1 / 6.0, 5 / 6.0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1 / 5.0, 4 / 5.0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1 / 5.0, 4 / 5.0},  
             });
 
-            var b = Vector<double>.Build.Dense(new double[] { -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-            var n = a.Transpose().Solve(b).ToArray();
+            var b = Vector<double>.Build.Dense(new double[] { -1, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
 
+            // Linear Equation System solving 
+            a = a.Transpose();
+            for (var i = 0; i < a.RowCount; i++)
+            {
+                a[i, i] = a[i, i] - 1;
+            }
+            var n = a.Solve(b).ToArray();
+
+            // Q
             var q = k.Select(((ki, i) => ki * n[i])).Sum();
 
+            // N1, N2, N3
             double n1 = 0;
             double n2 = 0;
             double n3 = 0;
@@ -42,6 +59,7 @@ namespace Lab1
                 }
             }
 
+            // Q1, Q2, Q3
             double q1 = 0;
             double q2 = 0;
             double q3 = 0;
@@ -69,9 +87,15 @@ namespace Lab1
             q2 = q2 * (1 / n2);
             q3 = q3 * (1 / n3);
 
+            // Q0
             var q0 = q / N;
 
-            Console.WriteLine("Q = " + q);
+            for (var i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine("n[" + i + "] = " + n[i]);
+            }
+
+            Console.WriteLine("\nQ = " + q);
 
             Console.WriteLine("N1 = " + n1);
             Console.WriteLine("N2 = " + n2);
